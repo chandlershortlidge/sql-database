f6e48ce527704751e3c07df206dcd651d4a8fc6a
Chandler Shortlidge <chandlershortlidge@gmail.com>
2025-07-24 14:28:28 +0200

    Update sql-database.sql
    -- Hypothesis 1: The Value of Style
    -- Artworks from certain styles (e.g., Impressionism, Cubism) consistently fetch higher average sale prices than works from other movements
    -- (e.g., Rococo, Classicism).
    -- outcome: top 10 styles by avg sale price:
    -- Classicism, American Landscape, Orientalism, Art Nouveau, Neo_Classicism, Rococo, Surrealism, American Art, Romantiscism, Naturalism

--- sql-database.sql
+++ sql-database.sql
@@ -1,8 +1,10 @@
 use famous_paintings;
 
+-- 1. exploring the data
 select * from canvas_size
 order by label desc;
 
+-- dimensions
 select 
 max(height) as max_height,
 max(width) as max_width,
@@ -16,7 +18,7 @@ avg(label) as avg_dimensions
 from canvas_size;
 
 
-
+-- price
 select sale_price, regular_price 
 from product_size;
 
@@ -65,5 +67,23 @@ from work
 group by style
 order by style_count desc; 
 
+-- Hypothesis 1: The Value of Style
+-- Artworks from certain styles (e.g., Impressionism, Cubism) consistently fetch higher average sale prices than works from other movements 
+-- (e.g., Rococo, Classicism).
+
+select
+	w.style,
+    count(distinct w.work_id) as style_count,
+	round(avg(ps.sale_price), 2) as avg_sale_price,
+	round(avg(ps.regular_price), 2) as avg_regular_price
+from work as w
+join product_size as ps on w.work_id = ps.work_id
+where style is NOT NULL
+group by w.style
+having style_count >= 100
+order by avg_sale_price desc
+limit 10;
+
+-- outcome: Classicism, American
 
 
