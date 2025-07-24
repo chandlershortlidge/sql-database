06f85e94c982b4cf82aa5b02424efd4dc92722fd
Chandler Shortlidge <chandlershortlidge@gmail.com>
2025-07-24 13:26:29 +0200

    Update sql-database.sql
    -- count number of painting subjects, order by min, max;  -- count number of painting styles, order by min, max

--- sql-database.sql
+++ sql-database.sql
@@ -28,3 +28,42 @@ min(regular_price) as min_regular_price,
 round(avg(sale_price), 2) as avg_sale_price,
 round(avg(regular_price), 2) as avg_regular_price
 from product_size;
+
+select * from subject;
+
+-- count number of painting subjects, order by min, max
+
+select
+	subject,
+	count(*) as subject_quantity
+from subject
+group by subject
+order by subject_quantity asc
+limit 1;
+
+select
+	subject,
+	count(*) as subject_quantity
+from subject
+group by subject
+order by subject_quantity desc
+limit 1;
+
+-- count number of painting styles, order by min, max
+
+select 
+	style, 
+	count(*) as style_count
+from work
+group by style
+order by style_count asc;
+
+select 
+	style, 
+	count(*) as style_count
+from work
+group by style
+order by style_count desc; 
+
+
+
