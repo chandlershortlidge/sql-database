5eb97fb32ff76696cb48ac13642c2d7b5bf09462
Chandler Shortlidge <chandlershortlidge@gmail.com>
2025-07-24 12:06:32 +0200

    Update sql-database.sql
    summarize canvas_size, product_size

--- sql-database.sql
+++ sql-database.sql
@@ -1,5 +1,30 @@
 use famous_paintings;
 
-select * from artist;
+select * from canvas_size
+order by label desc;
 
-select * from canvas_size;
+select 
+max(height) as max_height,
+max(width) as max_width,
+max(label) as max_dimensions,
+min(height) as min_height,
+min(width) as min_width,
+min(label) as min_dimensions,
+round(avg(height), 2) as avg_height,
+round(avg(width), 2) as avg_width,
+avg(label) as avg_dimensions
+from canvas_size;
+
+
+
+select sale_price, regular_price 
+from product_size;
+
+select 
+max(sale_price) as max_sale_price,
+max(regular_price) as max_regular_price,
+min(sale_price) as min_sale_price,
+min(regular_price) as min_regular_price,
+round(avg(sale_price), 2) as avg_sale_price,
+round(avg(regular_price), 2) as avg_regular_price
+from product_size;
