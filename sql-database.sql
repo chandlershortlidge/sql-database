1eda23338dc876bcb164dbb41d4101444a6d493b
Chandler Shortlidge <chandlershortlidge@gmail.com>
2025-07-25 13:35:37 +0200

    all hypotheses done

--- sql-database.sql
+++ sql-database.sql
@@ -172,6 +172,7 @@ sc.style_count asc;
 -- For example, do works by French or American artists command higher prices on average 
 -- than works by artists from other countries, even within the same style?
 
+-- create a CET with window function to filter by median price per style by nationality
 
 select  
 	nationality,
@@ -192,10 +193,44 @@ where w.style = "Impressionism"
 ) as nationality_by_sale_price
 WHERE rn = FLOOR(total_count/2);
 
-select style from work;
+-- then use stored procedure to view 5 most popular styles to see if meaningful differences exist
 
+call sale_price_by_nationality_style("Impressionism");
+call sale_price_by_nationality_style("Post-Impressionism"); 
+call sale_price_by_nationality_style("Realism");
 call sale_price_by_nationality_style("Baroque");
+call sale_price_by_nationality_style("Expressionism");
 
 
+-- 
 
+select  
+	nationality,
+	style, 
+	sale_price AS median_sale_price
+from  
+(
+select
+        w.style,
+        a.nationality,
+        ps.sale_price,
+		ROW_NUMBER() OVER (PARTITION BY w.style, a.nationality ORDER BY ps.sale_price) AS rn,
+		COUNT(*) OVER (PARTITION BY w.style, a.nationality) AS total_count
+from work as w
+	join product_size as ps on w.work_id = ps.work_id
+    join artist as a on w.artist_id = a.artist_id 
+ join (
+	select style
+    from work
+    group by style
+    order by count(*) desc
+    limit 5
+      ) as top_5_styles on w.style = top_5_styles.style
+) as nationality_by_sale_price
+WHERE rn = FLOOR(total_count/2)
+order by 
+style,
+median_sale_price desc;
+
+-- Error Code: 1235. This version of MySQL doesn't yet support 'LIMIT & IN/ALL/ANY/SOME subquery'
 
